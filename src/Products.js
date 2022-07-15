33a555ca0e02c3a5f3e6730c7dd9285c408176dd
Ram-Santosh <ram.santosh@techmojo.in>
2022-07-15 16:17:51 +0530

    Activity 1 is  done

---
+++ src/Products.js
@@ -0,0 +1,10 @@
+import productCat from "./products.json";
+
+export default function Products () {
+    console.log(productCat);
+    return (
+    <>
+        <h1>Products</h1>
+    </>
+    );
+}
