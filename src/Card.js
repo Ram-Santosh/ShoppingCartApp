d04518d81e41cb7f8820a14748e0f3c5fd457367
Ram-Santosh <ram.santosh@techmojo.in>
2022-07-18 12:34:52 +0530

    profile focus page completed

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,14 @@ import { useContext } from "react";
 import { passSelectedCard } from "./Products";
 import productCat from "./products.json";
 import Carousel from 'react-bootstrap/Carousel';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import logo from "./logo.svg";
+import Products from "./Products";
+import {root} from "./index";
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 var allProducts = productCat["products"];
 
@@ -12,27 +20,70 @@ export default function CardFocus () {
         if (product.id === selectedCard){
             return product;
         }
-    })
+    })[0];
 
+    var handleNav = () => {
+        root.render(
+            <React.StrictMode>
+                <Products />
+            </React.StrictMode>
+        );
+    }
+
+    console.log(product)
     return (
         <>
-            <Carousel style={{height:"290px", width: '500px'}}>
-                {product[0]["images"].map((image) => {
-                    var item = 
-                    <Carousel.Item interval={1500}>
-                        <img
-                        className="d-block"
-                        src={image}
-                        style={{height:"290px", width: '500px', objectFit:"cover"}}
-                        alt="First slide"
-                        />
-                    </Carousel.Item>
-                    return item;
-                    })
-                }  
-                
+            <Navbar bg="dark" variant="dark" className="mb-4">
+                <Container>
+                <Navbar.Brand onClick={handleNav}>
+                    <img
+                    alt=""
+                    src={logo}
+                    width="65"
+                    height="30"
+                    className="d-inline-block align-top"
+                    />
+                    Home
+                </Navbar.Brand>
+                </Container>
+            </Navbar>
 
-            </Carousel>
+            <Container>
+                <Row>
+                    <Col md={{span:5}}>
+                        <Carousel style={{height:"290px", width: '500px'}}>
+                            {product["images"].map((image, id) => {
+                                var item = 
+                                <Carousel.Item interval={1500} key={id}>
+                                    <img
+                                    className="d-block"
+                                    src={image}
+                                    style={{height:"290px", width: '500px', objectFit:"contain"}}
+                                    alt="First slide"
+                                    />
+                                </Carousel.Item>
+                                return item;
+                                })
+                            }  
+                        </Carousel>
+                    </Col>
+                    <Col md={{span:5, offset:2}}>
+                        <h2>{product.title}</h2>
+                        <p>Rating:  ⭐{product.rating}</p>
+                        <hr/>
+                        <p>M.R.P: <s>${product.price}</s></p>
+                        <p>On Sale: ${product.price - product.discountPercentage*product.price*0.01}</p>
+                    </Col>
+                </Row>
+                <Row className="mt-4">
+                    <hr />
+                    <h4>Product Description</h4>
+                    <p>Description: <i>{product.description}</i></p>
+                    <p>Brand: <i>{product.brand}</i></p>
+                    <p>Category: <i>{product.category}</i></p>
+                </Row>
+            </Container>
+            
         </>
     );
 }
--- src/Products.js
+++ src/Products.js
@@ -1,7 +1,5 @@
 import productCat from "./products.json";
-// import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-// import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Accordion from 'react-bootstrap/Accordion';
@@ -11,6 +9,9 @@ import { createContext, useState } from "react";
 import {root} from "./index";
 import React from 'react';
 import CardFocus from "./Card";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import logo from "./logo.svg";
 
 var passSelectedCard = createContext();
 
@@ -34,9 +35,31 @@ export default function Products () {
         );
     };
 
+    var handleNav = () => {
+        root.render(
+            <React.StrictMode>
+                <Products />
+            </React.StrictMode>
+        );
+    }
+
     return (
-    <>
-        {/* <Container> */}
+        <>
+            <Navbar bg="dark" variant="dark" className="mb-4">
+                <Container>
+                <Navbar.Brand onClick={handleNav}>
+                    <img
+                    alt=""
+                    src={logo}
+                    width="65"
+                    height="30"
+                    className="d-inline-block align-top"
+                    />
+                    Home
+                </Navbar.Brand>
+                </Container>
+            </Navbar>
+
             <Row>
                 <Col md={{span:2, offset:1}}>
                     <h3>Filter</h3>
@@ -88,7 +111,7 @@ export default function Products () {
                                 <Card 
                                     style={{ width: '18rem', marginBottom: "20px", height:"380px", borderRadius:"1rem"}} 
                                     key={id}>
-                                <Card.Img variant="top" src={image} style={{height:"18vh" , objectFit:"cover", borderTopLeftRadius:"1rem", borderTopRightRadius:"1rem"}}/>
+                                <Card.Img variant="top" src={image} style={{height:"18vh" , objectFit:"contain", borderTopLeftRadius:"1rem", borderTopRightRadius:"1rem"}}/>
                                 <Card.Body style={{backgroundColor:"#f3f3f3", borderBottomRightRadius:"1rem", borderBottomLeftRadius:"1rem"}}>
                                 <Card.Title>{title}</Card.Title>
                                 <Card.Text>Price: ${price}</Card.Text>
@@ -107,9 +130,7 @@ export default function Products () {
                     </Row>
                 </Col>
             </Row>
-            
-        {/* </Container> */}
-    </>
+        </>
     );
 };
 
