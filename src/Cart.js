5b117ef49ec63944ae3b4d2e1f22e102b6a2ec91
Ram-Santosh <ram.santosh@techmojo.in>
2022-07-26 11:03:24 +0530

    cart completed

--- src/Cart.js
+++ src/Cart.js
@@ -7,37 +7,34 @@ import Nav from 'react-bootstrap/Nav';
 import cartImg from "./cart.jpg";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
 
 export default function Cart () {
     var cart = useSelector((state) => state.cart)
     var navigate = useNavigate();
     var cartCount = useSelector((state)=> state.cartCount);
     var allProducts = useSelector((state) => state.allProducts);
+    var [displayItems, setDisplayItems] = useState([]);
+    var [totalPrice, setTotalPrice] = useState(0);
+
+    useEffect(()=> {
+        var total = 0;
+        var cards = allProducts.filter((item)=>{
+            if (item["id"] in cart) {
+                total += item.price*cart[item["id"]];
+                return item;
+            }
+        })
+        setTotalPrice(total);
+        setDisplayItems(cards);
+    },[]);
 
     var handleNav = () => {
         navigate("/Products");
     }
 
     var goToCart = () => {
-        var cards = allProducts.filter((item)=>{
-            if (item["id"] in cart) {
-                var card = <Row>
-                    <Col>
-                        <img src={item.thumbnail} alt="thumnail"></img>
-                    </Col>
-                    <Col>
-                        <h4>{item.title}</h4>
-                        <p>Stock: {item.stock}</p>
-                        <p>Quantity: {cart[item.id]}</p> 
-                    </Col>
-                    <Col>
-                    </Col>
-                </Row>;
-                // console.log(card)
-                return card;
-            }
-        })
-        console.log(cards)
         navigate("/Cart");
     }
 
@@ -63,8 +60,31 @@ export default function Cart () {
             </Navbar>
             <Container>
                 <h3>Shopping Cart</h3>
-
-                {/* {} */}
+                <Row>
+                    <Col md={{span:9}}>
+                        {displayItems.map((item)=>{
+                            var card = <Row className="p-4 mb-2" style={{border: "outset"}}>
+                            <Col>
+                                <img src={item.thumbnail} style={{width:"230px", height:"200px", objectFit:"contain"}} alt="thumnail"></img>
+                            </Col>
+                            <Col>
+                                <h4>{item.title}</h4>
+                                <p>Stock: {item.stock}</p>
+                                <p>Quantity: {cart[item.id]}</p> 
+                            </Col>
+                            <Col>
+                                <h4>Price</h4>
+                                <p>{item.price}</p>
+                            </Col>
+                        </Row>;
+                        return card;
+                        })}
+                    </Col>
+                    <Col md={{span:3}} className="p-3">
+                        <h3>SubTotal: ${totalPrice}</h3>  
+                        <Button variant="success" className="m-2">Proceed to Checkout</Button>
+                    </Col>
+                </Row>
             </Container>
         </>
     )
